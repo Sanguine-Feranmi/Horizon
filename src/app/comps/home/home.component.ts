6fa72cef3209e7411ac8e2dd179105e1cb2bd968
Sanguine Feranmi <feranmibayeri@gmail.com>
2024-10-29 13:25:15 +0000

    jpro

--- src/app/comps/home/home.component.ts
+++ src/app/comps/home/home.component.ts
@@ -6,7 +6,22 @@ import { Component, Input } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  name: string = 'Sanguine';
+  details : any=
+  {
+    name: "Sanguine",
+    fullname : "Sanguine Feranmi",
+    mail : "feranmibayeri@gmail.com"
+  }
+  bankD: any=
+  {
+    name:'',
+    NBA: 2,
+    curBal: 2698.12
+  }
+  bank: any =
+  {
+    chase: 'Chase Bank'
+  }
   chase: string = 'Chase Bank'
   NBA: number = 2;
   curBal: string = "2,698.12";
